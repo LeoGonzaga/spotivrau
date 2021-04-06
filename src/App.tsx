19674c6b6ceb13f6dfb99bbdfbd5b8bd9db9fd56
LeoGonzaga <sacredlotus05@gmail.com>
2021-04-06 00:00:58 -0300

    criando requisição para a api do youtube e exibindo os resultados dentro do um iframe

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from "react";
+
+type Video = {
+  id?: {
+    videoId: string;
+  };
+};
 
 function App() {
+  const [text, setText] = useState("");
+  const [videos, setVideos] = useState([]);
+  const handleSubmit = useCallback(async () => {
+    try {
+      let res = await fetch(
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&key=SUAKEY&type=video&q=" +
+          text
+      );
+      let resJSON = await res.json();
+      setVideos(resJSON.items);
+    } catch (e) {
+      console.log(e);
+    }
+  }, [text]);
+
+  console.log(text);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={text}
+        onChange={({ target }) => setText(target.value)}
+      />
+      <button onClick={handleSubmit}>Buscar</button>
+      {videos &&
+        videos.map((video: Video) => (
+          <iframe
+            width="853"
+            height="480"
+            src={`https://www.youtube.com/embed/${video.id?.videoId}`}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="Embedded youtube"
+          />
+        ))}
     </div>
   );
 }
