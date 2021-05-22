1761c7b3a9ecd0c2b7f6531884acf0c85e3534a6
LeoGonzaga <sacredlotus05@gmail.com>
2021-05-22 00:13:39 -0300

    melhorando o layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React, { useCallback, useState } from "react";
 import {
+  Click,
   Container,
   ContainerPlayer,
+  GlobalStyle,
   ListMusic,
   Player,
   SearchInput,
@@ -36,56 +38,52 @@ function App() {
   console.log(videos);
 
   return (
-    <Container>
-      <WrapperSearch>
-        <SearchInput
-          type="search"
-          value={text}
-          placeholder="O que você quer ouvir?"
-          onChange={({ target }) => setText(target.value)}
-        />
-        <button onClick={handleSubmit}>Buscar</button>
-      </WrapperSearch>
+    <>
+      <GlobalStyle />
+      <Container>
+        <WrapperSearch>
+          <SearchInput
+            type="search"
+            value={text}
+            placeholder="O que você quer ouvir?"
+            onChange={({ target }) => setText(target.value)}
+          />
+          <button onClick={handleSubmit}>Buscar</button>
+        </WrapperSearch>
 
-      <ContainerPlayer>
-        <Player>
-          {videos && (
-            <iframe
-              width="400"
-              height="480"
-              src={`https://www.youtube.com/embed/${id}`}
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              title="Embedded youtube"
-            />
-          )}
-        </Player>
-
-        <ListMusic>
-          <ul>
-            {videos &&
-              videos.map((video: Video) => (
-                <button onClick={() => setId(video.id?.videoId)}>
-                  <li>{video.snippet.title.substring(0, 40)}</li>
-                </button>
-              ))}
-          </ul>
-          {/* {videos &&
-            videos.map((video: Video) => (
+        <ContainerPlayer>
+          <Player>
+            {id && (
               <iframe
                 width="400"
-                height="480"
-                src={`https://www.youtube.com/embed/${video.id?.videoId}`}
+                height="400"
+                src={`https://www.youtube.com/embed/${id}`}
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
                 title="Embedded youtube"
               />
-            ))} */}
-        </ListMusic>
-      </ContainerPlayer>
-    </Container>
+            )}
+          </Player>
+
+          <ListMusic>
+            <ul>
+              {videos &&
+                videos.map((video: Video, index) => (
+                  <Click
+                    onClick={() => setId(video.id?.videoId)}
+                    selected={id == video.id.videoId}
+                  >
+                    <li>
+                      {index} {video.snippet.title.substring(0, 35)}
+                    </li>
+                  </Click>
+                ))}
+            </ul>
+          </ListMusic>
+        </ContainerPlayer>
+      </Container>
+    </>
   );
 }
 
--- src/styled.tsx
+++ src/styled.tsx
@@ -1,5 +1,13 @@
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background: #0D081F;
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+  }
+`;
 export const Container = styled.div`
   width: 100%;
   height: 100%;
@@ -10,64 +18,85 @@ export const Container = styled.div`
   justify-content: space-around;
   margin: 0;
   padding: 0;
+
+  background-image: url("https://bipedepop.files.wordpress.com/2018/08/bc3adpede-pop-para-reflexc3a3o-e-relaxamento-conhec3a7a-lofi-hip-hop-um-estilo-para-introspectividade-e-para-vida-2.gif?w=1200");
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
 
 export const WrapperSearch = styled.div`
   display: flex;
+  align-items: center;
   margin: 20px 0;
 
   button {
     border: 1px solid #000;
     background-color: #fff;
     height: 35px;
-    width: 150px;
+    width: 100px;
     margin-left: 10px;
     border-radius: 8px;
   }
 `;
 
 export const SearchInput = styled.input`
-  color: #ccc;
+  color: #0D081F;
+  background-color: #ffff;
   border: none;
   outline: 0;
-  border: 1px solid #ccc;
-  padding: 10px;
+  padding: 10px 25px;
   width: 400px;
-  border-radius: 8px;
+  border-radius: 20px;
+  height: 35px;
+  font-size: 15px;
 `;
 
 export const ContainerPlayer = styled.div`
   display: flex;
-  width: 800px;
+  width: 100%;
   justify-content: center;
   align-items: center;
 `;
 
 export const Player = styled.div`
-  background-color: #fff;
-  border-radius: 30px;
-  width: 50%;
   margin: 0 30px;
-  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+
+  iframe {
+    border-radius: 30px;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  }
 `;
 
+type Props = {
+  selected: boolean;
+};
 export const ListMusic = styled.div`
   height: 500px;
+  width: 50%;
   overflow-y: scroll;
+  overflow-x: hidden;
   display: flex;
   flex-direction: column;
-
-  button{
-    width: 100%;
-    display: flex;
-    align-items: center;
-    padding: 0;
-    margin-bottom:10px;
-
+  &::-webkit-scrollbar {
+    width: 0px;
   }
+
   li {
     list-style: none;
     padding: 10px;
-    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
   }
+`;
+
+export const Click = styled.button`
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  padding: 5px 0;
+  margin-bottom: 10px;
+  outline: none;
+  border-radius: 4px;
+  background-color: ${(props: Props) => (props.selected ? "#0D081F" : "#fff")};
+  color: ${(props: Props) => (props.selected ? "#fff" : "#0D081F")};
+  border: none;
 `;
